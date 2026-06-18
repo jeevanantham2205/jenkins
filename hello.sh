da43c8b7cda5e4fd7bfb7c15bdc4095affcb0217
Jeevanantham S <your-email@example.com>
2026-06-18 10:00:41 +0000

    Fix merge conflict in hello.sh

--- hello.sh
+++ hello.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-<<<<<<< HEAD
+
 echo "Hello from Jenkins!"
+echo "Build triggered successfully."
 date
 hostname
-=======
-echo "Hello from Jenkins"
-date
->>>>>>> 456d220b6f04c299370e6ad29f56d5f2b8de46c9
-#!/bin/bash
-<<<<<<< HEAD
-echo "Hello from Jenkins!"
-date
-hostname
-=======
-echo "Hello from Jenkins"
-date
->>>>>>> 456d220b6f04c299370e6ad29f56d5f2b8de46c9
+
+
